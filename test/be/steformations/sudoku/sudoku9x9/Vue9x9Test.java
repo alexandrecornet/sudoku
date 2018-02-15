88dc20ac29c6bf52bdd678b26d9f578b0f4231b9
yannick <yannick.boogaerts@ulg.ac.be>
2018-02-15 17:04:42 +0100

    Vue9X9.afficheGrilleVide() affiche une grille vide d'un sudoku 9x9. 	"      SUDOKU 9 x 9      " 	"      ============      " 	"+-------+-------+-------+" 	"| . . . | . . . | . . . |" 	"| . . . | . . . | . . . |" 	"| . . . | . . . | . . . |" 	"+-------+-------+-------+" 	"| . . . | . . . | . . . |" 	"| . . . | . . . | . . . |" 	"| . . . | . . . | . . . |" 	"+-------+-------+-------+" 	"| . . . | . . . | . . . |" 	"| . . . | . . . | . . . |" 	"| . . . | . . . | . . . |" 	"+-------+-------+-------+"

---
+++ test/be/steformations/sudoku/sudoku9x9/Vue9x9Test.java
@@ -0,0 +1,61 @@
+package be.steformations.sudoku.sudoku9x9;
+
+import java.util.Scanner;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import be.steformations.util.Scenario;
+import be.steformations.util.TestScenario;
+
+public class Vue9x9Test {
+
+	/**
+	 * Vue9X9.afficheGrilleVide() affiche une grille vide d'un sudoku 9x9.
+	 * 
+	 * <pre>
+	"      SUDOKU 9 x 9      "
+	"      ============      "
+	"+-------+-------+-------+"
+	"| . . . | . . . | . . . |"
+	"| . . . | . . . | . . . |"
+	"| . . . | . . . | . . . |"
+	"+-------+-------+-------+"
+	"| . . . | . . . | . . . |"
+	"| . . . | . . . | . . . |"
+	"| . . . | . . . | . . . |"
+	"+-------+-------+-------+"
+	"| . . . | . . . | . . . |"
+	"| . . . | . . . | . . . |"
+	"| . . . | . . . | . . . |"
+	"+-------+-------+-------+"
+	 * </pre>
+	 */
+	@Test
+	public void testafficheGrilleVide(){
+		Vue9X9 vue9x9 = new Vue9X9();
+		Scenario scenario = new Scenario();
+		scenario.setApplication(() -> vue9x9.afficheGrilleVide());
+		scenario.test(0, new TestScenario() {
+			@Override
+			public void test(String output) {
+				Scanner scanner = new Scanner(output);
+				Assert.assertEquals("      SUDOKU 9 x 9      ", scanner.nextLine()); 
+				Assert.assertEquals("      ============      ", scanner.nextLine()); 
+				Assert.assertEquals("+-------+-------+-------+", scanner.nextLine()); 
+				Assert.assertEquals("| . . . | . . . | . . . |", scanner.nextLine()); 
+				Assert.assertEquals("| . . . | . . . | . . . |", scanner.nextLine()); 
+				Assert.assertEquals("| . . . | . . . | . . . |", scanner.nextLine()); 
+				Assert.assertEquals("+-------+-------+-------+", scanner.nextLine()); 
+				Assert.assertEquals("| . . . | . . . | . . . |", scanner.nextLine()); 
+				Assert.assertEquals("| . . . | . . . | . . . |", scanner.nextLine()); 
+				Assert.assertEquals("| . . . | . . . | . . . |", scanner.nextLine()); 
+				Assert.assertEquals("+-------+-------+-------+", scanner.nextLine()); 
+				Assert.assertEquals("| . . . | . . . | . . . |", scanner.nextLine()); 
+				Assert.assertEquals("| . . . | . . . | . . . |", scanner.nextLine()); 
+				Assert.assertEquals("| . . . | . . . | . . . |", scanner.nextLine()); 
+				Assert.assertEquals("+-------+-------+-------+", scanner.nextLine()); 
+			}
+		});
+	}
+}
