c15a2b5c8c8381df6e8303984dbe19d050e2657a
yannick <yannick.boogaerts@ulg.ac.be>
2018-02-20 15:24:25 +0100

    Vue4x4.afficheGrilleVide() affiche une grille vide d'un sudoku 4x4. 	" SUDOKU 4 x 4" 	" ============" 	"+-----+-----+" 	"| . . | . . |" 	"| . . | . . |" 	"+-----+-----+" 	"| . . | . . |" 	"| . . | . . |" 	"+-----+-----+"

---
+++ test/be/steformations/sudoku/sudoku4x4/Vue4x4Test.java
@@ -0,0 +1,49 @@
+package be.steformations.sudoku.sudoku4x4;
+
+import java.util.Scanner;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import be.steformations.util.Scenario;
+import be.steformations.util.TestScenario;
+
+public class Vue4x4Test {
+
+	/**
+	 * Vue4x4.afficheGrilleVide() affiche une grille vide d'un sudoku 4x4.
+	 * 
+	 * <pre>
+	" SUDOKU 4 x 4"
+	" ============"
+	"+-----+-----+"
+	"| . . | . . |"
+	"| . . | . . |"
+	"+-----+-----+"
+	"| . . | . . |"
+	"| . . | . . |"
+	"+-----+-----+"
+	 * </pre>
+	 */
+	@Test
+	public void testafficheGrilleVide(){
+		Vue4x4 vue4x4 = new Vue4x4();
+		Scenario scenario = new Scenario();
+		scenario.setApplication(() -> vue4x4.afficheGrilleVide());
+		scenario.test(0, new TestScenario() {
+			@Override
+			public void test(String output) {
+				Scanner scanner = new Scanner(output);
+				Assert.assertEquals(" SUDOKU 4 x 4", scanner.nextLine()); 
+				Assert.assertEquals(" ============", scanner.nextLine()); 
+				Assert.assertEquals("+-----+-----+", scanner.nextLine()); 
+				Assert.assertEquals("| . . | . . |", scanner.nextLine()); 
+				Assert.assertEquals("| . . | . . |", scanner.nextLine()); 
+				Assert.assertEquals("+-----+-----+", scanner.nextLine()); 
+				Assert.assertEquals("| . . | . . |", scanner.nextLine()); 
+				Assert.assertEquals("| . . | . . |", scanner.nextLine()); 
+				Assert.assertEquals("+-----+-----+", scanner.nextLine()); 
+			}
+		});
+	}
+}
